813836d969fe072f02efe357f298622928af70a7
energybo <ansqhrua@gmail.com>
2019-03-30 21:53:59 +0900

    index file added

--- src/test/java/com/sadang/app/WorkoutTracker/WorkoutTrackerApplicationTests.java
+++ src/test/java/com/sadang/app/WorkoutTracker/WorkoutTrackerApplicationTests.java
@@ -3,14 +3,25 @@ package com.sadang.app.WorkoutTracker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Controller
 public class WorkoutTrackerApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@GetMapping("")
+
+	public String hello() {
+
+		return "index";
+	}
 }
+
+
